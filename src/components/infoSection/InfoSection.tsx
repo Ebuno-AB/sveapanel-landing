4b21350e025c5957bdb798c0074dc99a7d4689f0
hkrishan <hugo@ebuno.net>
2025-09-18 12:30:49 +0200

    Update InfoSection content and improve CSS formatting
    Added Swish payout info to the 'Tjäna pengar' card in InfoSection. Adjusted .info-section-cards alignment in InfoSection.css and improved formatting, consistency, and minor style tweaks in RatingsSection.css.

--- src/components/infoSection/InfoSection.tsx
+++ src/components/infoSection/InfoSection.tsx
@@ -1,28 +1,27 @@
-import React from 'react';
-import './InfoSection.css';
-import CardSection from '../cardSection/CardSection';
+import React from "react";
+import "./InfoSection.css";
+import CardSection from "../cardSection/CardSection";
 
 const InfoSection: React.FC = () => {
   return (
     <div className="info-section">
-      <h2 style={{ color: "white", marginTop: "3rem"}}>
+      <h2 style={{ color: "white", marginTop: "3rem" }}>
         Tre enkla steg till att tjäna pengar
       </h2>
 
       <div className="info-section-cards">
-       <CardSection
-        title="Spela mobilspel"
-        description="Spela och samla poäng – belöningarna tickar in medan du spelar."
-      />
-      <CardSection
-        title="Svara på enkäter"
-        description="Korta enkäter anpassade för dig. Få poäng för varje svar och extra bonus när du tipsar en vän."
-      />
-      <CardSection
-        title="Tjäna pengar"
-        description="Lös uppgifter, klara mål och se saldot växa."
-      />
-
+        <CardSection
+          title="Spela mobilspel"
+          description="Spela och samla poäng – belöningarna tickar in medan du spelar."
+        />
+        <CardSection
+          title="Svara på enkäter"
+          description="Korta enkäter anpassade för dig. Få poäng för varje svar och extra bonus när du tipsar en vän."
+        />
+        <CardSection
+          title="Tjäna pengar"
+          description="Lös uppgifter, klara mål och se saldot växa. Ta ut dina pengar direkt till Swish!"
+        />
       </div>
     </div>
   );
