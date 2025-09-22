15060dd668acdf44bbfd47ae4f6f2d17f65681c1
OliverWeitman <oliver@ebuno.net>
2025-09-22 16:29:14 +0200

    build updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "react/public",
+          dest: "react"
+        }
+      ]
+    })
+  ],
+  build: {
+    outDir: "dist",       // keep index.html in root
+    assetsDir: "react/bundle/assets", // put everything else in here
+    emptyOutDir: true
+  },
   server: {
     proxy: {
       // Proxy API calls to your production backend
