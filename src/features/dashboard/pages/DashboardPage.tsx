c58906d944b8b9f9386218f564bc260bd91f0d6f
EliasPleijelMurray <elias.pleijel@gmail.com>
2026-03-19 13:01:40 +0100

    css fix

--- src/features/dashboard/pages/DashboardPage.tsx
+++ src/features/dashboard/pages/DashboardPage.tsx
@@ -1,15 +1,19 @@
 import "@/features/dashboard/styles/DashboardPage.css";
 import { DashboardSurveys } from "@/features/dashboard/components/DashboardSurveys";
 import { DashboardStores } from "../components/DashboardStores";
+import Footer from "@/components/footer/Footer";
 
 const DashboardPage = () => {
   return (
-    <div className="dashboard">
-      <div className="dashboard-content">
-        <DashboardSurveys />
-        <DashboardStores />
+    <>
+      <div className="dashboard">
+        <div className="dashboard-content">
+          <DashboardSurveys />
+          <DashboardStores />
+        </div>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 };
 
