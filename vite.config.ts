f2e24f0e786c9852f0b89f47a2ee3a365ba08c34
EliasPleijelMurray <elias.pleijel@gmail.com>
2026-03-30 16:07:43 +0200

    feat: update referral link generation and improve referral code handling

--- vite.config.ts
+++ vite.config.ts
@@ -30,8 +30,9 @@ export default defineConfig({
         secure: true,
       },
       "/v1": {
-        target: "http://192.168.1.112:3025",
+        target: "https://api.sveapanelen.yflow.io/v1",
         changeOrigin: true,
+        secure: true,
       },
     },
   },
